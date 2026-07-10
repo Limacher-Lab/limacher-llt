1ceabc90395315cd05946a7d87be773d531dfafa
ejlimach <ejlimach@ucalgary.ca>
2026-07-10 16:17:37 -0600

    testing-testing
    cleaned up comments in test script for main liftingline.m.  Changed angle of attack limits for test.

--- test_liftingline.m
+++ test_liftingline.m
@@ -5,26 +5,8 @@ clear; clc; close all;
 forcefile = "xf-n0012-il-100000_truncated.csv";
 wingfilename = "blade_elliptic.txt";
 
-% [~,~,y,Gamma,v] = liftingline(wingfilename,forcefile,2);
-% 
-% figure();
-% yyaxis left;
-% plot(y,Gamma,'b.-');
-% ylim([0,1.1*max(Gamma)]);
-% yyaxis right; 
-% plot(y,v,'r.-');
-% ylim([-1.1*max(abs(v)), 1.1*max(abs(v))])
-% leg = legend('$\Gamma$','$v$');
-% xlabel('$y^*$','interpreter','latex');
-% set(leg,'interpreter','latex','location','northoutside');
-% grid on;
-% set(gcf,'color','w');
-
-
-%%
-
  
-alfas = [-20:20];
+alfas = [-22:22];
 [CL,CD,y,Gamma,v] = liftingline(wingfilename,forcefile,alfas);
 
 figure();
